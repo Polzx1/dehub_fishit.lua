1942feb8064cea0211b8245ac75f6db5ef64874d
DeHub <ifanpbg3@gmail.com>
2026-02-06 08:21:19 +0700

    Update URL to point to DeHubMain.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,6 @@
 --// DeHub Loader (Final)
 
-local url = "https://raw.githubusercontent.com/Polzx1/dehub_fishit.lua/main/Loader.lua"
+local url = "https://raw.githubusercontent.com/Polzx1/dehub_fishit.lua/main/DeHubMain.lua"
 
 pcall(function()
     loadstring(game:HttpGet(url))()
